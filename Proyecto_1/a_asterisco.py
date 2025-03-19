5b26deb964dec5b8bfc25a600a5c0de56a154530
danielNR32 <daniel.nr2001tec@gmail.com>
2025-03-19 11:40:15 -0600

    haarcascade

---
+++ Proyecto_1/a_asterisco.py
@@ -0,0 +1,107 @@
+import pygame
+from queue import PriorityQueue
+import time
+
+# Configuraciones iniciales
+ANCHO_VENTANA = 600
+VENTANA = pygame.display.set_mode((ANCHO_VENTANA, ANCHO_VENTANA))
+pygame.display.set_caption("Visualización de Nodos")
+
+# Colores (RGB)
+BLANCO = (255, 255, 255)
+NEGRO = (0, 0, 0)
+GRIS = (128, 128, 128)
+VERDE = (0, 255, 0)
+ROJO = (255, 0, 0)
+NARANJA = (255, 165, 0)
+PURPURA = (128, 0, 128)
+AZUL = (0, 0, 255)
+
+pygame.font.init() 
+FUENTE = pygame.font.SysFont("Arial", 10,)  
+
+
+
+
+
+class Nodo:
+    def __init__(self, fila, col, ancho, total_filas):
+        self.fila = fila
+        self.col = col
+        self.x = fila * ancho
+        self.y = col * ancho
+        self.color = BLANCO
+        self.ancho = ancho
+        self.total_filas = total_filas
+        self.g = float("inf")  
+        self.h = 0 
+        self.f = float("inf")
+        self.father = None  
+
+    #Obtener la fila y columna del nodo actual
+    def get_pos(self):
+        return self.fila, self.col
+
+    def es_pared(self):
+        return self.color == NEGRO
+
+    def es_inicio(self):
+        return self.color == NARANJA
+
+    def es_fin(self):
+        return self.color == PURPURA
+    
+    def restablecer(self):
+        self.color = BLANCO
+
+    def hacer_inicio(self):
+        self.color = NARANJA
+
+    def hacer_pared(self):
+        self.color = NEGRO
+
+    def hacer_fin(self):
+        self.color = PURPURA
+
+    def buscar(self):
+        self.color = AZUL
+
+    def ruta_final(self):
+        self.color = ROJO
+
+
+    def dibujar(self, ventana):
+        pygame.draw.rect(ventana, self.color, (self.x, self.y, self.ancho, self.ancho))
+
+def crear_grid(filas, ancho):
+    grid = []
+    ancho_nodo = ancho // filas
+    for i in range(filas):
+        grid.append([])
+        for j in range(filas):
+            nodo = Nodo(i, j, ancho_nodo, filas)
+            grid[i].append(nodo)
+    return grid
+
+def dibujar_grid(ventana, filas, ancho):
+    ancho_nodo = ancho // filas
+    for i in range(filas):
+        pygame.draw.line(ventana, GRIS, (0, i * ancho_nodo), (ancho, i * ancho_nodo))
+        for j in range(filas):
+            pygame.draw.line(ventana, GRIS, (j * ancho_nodo, 0), (j * ancho_nodo, ancho))
+
+def dibujar(ventana, grid, filas, ancho):
+    ventana.fill(BLANCO)
+    for fila in grid:
+        for nodo in fila:
+            nodo.dibujar(ventana)
+
+    dibujar_grid(ventana, filas, ancho)
+    pygame.display.update()
+
+def obtener_click_pos(pos, filas, ancho):
+    ancho_nodo = ancho // filas
+    y, x = pos
+    fila = y // ancho_nodo
+    col = x // ancho_nodo
+    return fila, col
