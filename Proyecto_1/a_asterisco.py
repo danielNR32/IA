bbbb9682db78b4ce19a05ffad00db2b8e9f23850
danielNR32 <daniel.nr2001tec@gmail.com>
2025-04-02 10:01:41 -0600

    Archivo richi

--- Proyecto_1/a_asterisco.py
+++ Proyecto_1/a_asterisco.py
@@ -105,3 +105,143 @@ def obtener_click_pos(pos, filas, ancho):
     fila = y // ancho_nodo
     col = x // ancho_nodo
     return fila, col
+
+
+def main(ventana, ancho):
+    FILAS = 11
+    grid = crear_grid(FILAS, ancho)
+    inicio = None
+    fin = None
+    corriendo = True
+
+    while corriendo:
+        dibujar(ventana, grid, FILAS, ancho)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                corriendo = False
+
+            if pygame.mouse.get_pressed()[0]:  # Left click
+                pos = pygame.mouse.get_pos()
+                fila, col = obtener_click_pos(pos, FILAS, ancho)
+                nodo = grid[fila][col]
+                
+                if not inicio and nodo != fin:
+                    inicio = nodo
+                    inicio.hacer_inicio()
+
+                elif not fin and nodo != inicio:
+                    fin = nodo
+                    fin.hacer_fin()
+
+                elif nodo != fin and nodo != inicio:
+                    nodo.hacer_pared()
+
+            elif pygame.mouse.get_pressed()[2]:  # Click derecho
+                pos = pygame.mouse.get_pos()
+                fila, col = obtener_click_pos(pos, FILAS, ancho)
+                nodo = grid[fila][col]
+                nodo.restablecer()     
+
+                if nodo == inicio:
+                    inicio = None
+                elif nodo == fin:
+                    fin = None
+
+            elif pygame.key.get_pressed()[pygame.K_SPACE]:
+                algoritmo(inicio, fin, grid, FILAS)
+                
+
+    pygame.quit()
+
+
+def heuristica(nodo, fin):
+    # Obtener las posiciones del nodo actual y del nodo final
+    x1, y1 = nodo.get_pos()
+    x2, y2 = fin.get_pos()
+    distancia_x = abs(x2 - x1)
+    distancia_y = abs(y2 - y1)
+    distancia_total = distancia_x + distancia_y
+    heuristica = distancia_total * 10
+
+    return heuristica
+
+
+
+def vecinos(nodo, grid):
+    vecinos = []
+
+    direcciones = [
+        (1, 0), (-1, 0), (0, 1), (0, -1), 
+        (1, 1), (1, -1), (-1, 1), (-1, -1)
+    ]
+    
+    for direccion in direcciones:
+        nueva_fila = nodo.fila + direccion[0]
+        nueva_col = nodo.col + direccion[1]
+        
+        if 0 <= nueva_fila < nodo.total_filas and 0 <= nueva_col < nodo.total_filas:
+            vecino = grid[nueva_fila][nueva_col]
+            if not vecino.es_pared():
+                vecinos.append(vecino)
+
+    return vecinos
+
+def mostrar_ruta(current):
+    while current.father:
+        current = current.father
+        current.ruta_final()
+
+
+   
+
+def algoritmo(inicio, fin, grid, filas):
+    contador = 0
+    lista_abierta = PriorityQueue()
+    lista_abierta.put((0, contador, inicio))
+    lista_cerrada = set()
+    
+    inicio.g = 0
+    inicio.f = heuristica(inicio, fin)
+    
+    while not lista_abierta.empty():
+        current = lista_abierta.get()[2]
+        lista_cerrada.add(current)
+
+        if current == fin:
+            mostrar_ruta(fin)
+            return True
+        
+        for vecino in vecinos(current, grid):
+            if vecino in lista_cerrada:
+                continue
+
+
+               
+    
+            if (current.fila != vecino.fila):
+                peso = 14
+            else:
+                peso = 10     
+           
+            temporal_g = current.g + peso
+
+            if temporal_g < vecino.g:
+                vecino.father = current
+                vecino.g = temporal_g
+                vecino.h = heuristica(vecino, fin)
+                vecino.f = vecino.g + vecino.h
+
+                if vecino not in [i[2] for i in lista_abierta.queue]:
+                    contador += 1
+                    lista_abierta.put((vecino.f, contador, vecino))
+                    if vecino != fin:
+                        vecino.buscar()
+        
+        if current != inicio:
+            current.buscar()
+        dibujar(VENTANA, grid, filas, ANCHO_VENTANA)
+        time.sleep(0.1) 
+    return False
+
+
+main(VENTANA, ANCHO_VENTANA)
